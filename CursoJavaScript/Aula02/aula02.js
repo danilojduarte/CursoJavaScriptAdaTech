5e411ec3490e7446e34e86de8ec903e8f500b7f7
DaniloDuarte <danilojoseduarte@gmail.com>
2023-05-26 15:40:12 -0300

    Aula 02 (Tipos de dados) - Concluída

---
+++ CursoJavaScript/Aula02/aula02.js
@@ -0,0 +1,25 @@
+// Aula 02 - Variaveis
+
+// Criado primeira variavel
+let idade = 38
+
+console.log(idade);
+
+// Tipos de Variaveis
+
+// number
+let numero = 38; 
+// number
+let altura = 177;
+// String
+let nome = 'Danilo';
+// booleano
+let estudando = true
+
+console.log(numero, typeof numero);
+console.log(altura, typeof altura);
+console.log(nome, typeof nome);
+console.log(estudando, typeof estudando);
+
+
+//  Tipos de Variaveis (let, var e const(const não poderá alterar o valor))
