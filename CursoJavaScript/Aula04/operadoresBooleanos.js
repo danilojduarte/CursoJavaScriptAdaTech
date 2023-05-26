2b97f546f57e876d757caf99b6d57783e524dc18
DaniloDuarte <danilojoseduarte@gmail.com>
2023-05-26 16:11:02 -0300

    Aula 04 (// Operadores Booleanos), concluida

---
+++ CursoJavaScript/Aula04/operadoresBooleanos.js
@@ -0,0 +1,28 @@
+// Operadores Booleanos
+
+// Igualdade : == (ou ===)
+// Desigualdade : != (ou !==)
+// Maior que : >
+// Maior ou igual >=
+// Menor que : <
+// Menor ou igual <=
+// 
+// Conjuções Lógicas
+
+// and => &&
+let idade = 26
+let cnh = true;
+const possoDirigir = idade >= 18 && cnh == true
+//OR => ||
+idade = 40;
+const voto = idade < 18 || idade >=70;
+//NOT => !
+const gostoCurso = false;
+
+console.log(!gostoCurso)
+
+const numero = 10
+
+console.log(numero == "10")
+// JavaScript entende que sim, é um número mesmo sendo string
+// Compara tipos de variaveis, utilizar (===);
