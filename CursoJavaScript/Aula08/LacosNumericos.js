8839463d718f8f12ac3cba1cdd626ed8d3ee8421
DaniloDuarte <danilojoseduarte@gmail.com>
2023-05-27 14:40:22 -0300

    Aula (Laços Numéricos), concluída

---
+++ CursoJavaScript/Aula08/LacosNumericos.js
@@ -0,0 +1,39 @@
+// Laços Numéricos
+
+// Problema
+
+const nota1 = Number(input.question('Informe a nota 1: '))
+const nota2 = Number(input.question('Informe a nota 2: '))
+const nota3 = Number(input.question('Informe a nota 3: '))
+
+let media = (nota1 + nota2 + nota3) / 3;
+
+// Acumulador
+let acumulador = 0;
+
+acumulador = acumulador + 10
+
+acumulador +2
+acumulador++
+
+// Estrutura FOR
+
+for (let i = 0; i <= 4; i++) {
+    console.log('Repetição', i)
+}
+
+for (let i = 12; i > 8; i--) {
+    console.log('Repetição', i)
+}
+
+// Resolvendo
+let nota;
+let soma = 0;
+
+for (let i = 1; i <= 3; i++) {
+    nota= Number(input.question(`Informar a nota ${i} do aluno`))
+
+    soma = soma + nota
+}
+
+console.log(`A media do aluno é${soma / 3}.`)
